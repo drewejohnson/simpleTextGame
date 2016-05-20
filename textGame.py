d0a6da52951f5b525c73437056a4a8270eff17a1
drewejohnson <1drew.e.johnson@gmail.com>
2016-05-20 09:25:52 -0400

    New Driver
    textGame.py did not commit with previous commit getInput() and verbDict
    move

--- textGame.py
+++ textGame.py
@@ -15,30 +15,6 @@ A simple text-based game where you explore a maze, fight monsters, and obtain ge
 import verbFuncMod as VFM
 import classMod as CM
 
-#-----------
-# Functions for handing user commands
-#-----------	
-def getInput():
-	command = input(": ").split()
-	verbIn = command[0]
-	if verbIn in verbDict:
-		verb = verbDict[verbIn]
-	else:
-		print("Unknown verb {}".format(verbIn))
-		return
-	if len(command)>=2:
-		nounIn = command[1]
-		print(verb(nounIn))
-	else:
-		print(verb())
-		
-verbDict = {
-	"say": VFM.say,
-	"examine": VFM.examine,
-	"hit": VFM.hit,
-	"help":VFM.help,
-}
-
 #------------
 # Creatures
 #------------
@@ -50,4 +26,4 @@ pName = input("What is your name, brave adventurer: \n")
 player = CM.Adventurer(pName)
 print("And so, "+pName+' departed on their brave quest into the unknown!')
 while True:
-	getInput()
+	VFM.getInput()
